ce5058c75c437fb399e4f11654cdfced86cd6939
Alejandra Mensaque <99336987+alemenfon@users.noreply.github.com>
2022-11-16 20:52:09 +0100

    Update ej11.py

--- programacionModular/src/ej11.py
+++ programacionModular/src/ej11.py
@@ -3,15 +3,15 @@ Escribe una función intersect que reciba dos listas y devuelva otra lista con l
 elementos que son comunes a ambas, sin repetir ninguno
 '''
 
-lista1=[1,2,3,4,5,6,7,8,9]
+lista1=[1,2,5,4,5,6,7,8,9,9]
 lista2=[1,3,5,6,7,9]
 def intersect (lista1,lista2):
     comunes=[]
+    definitiva=[]
     for i in range(len(lista1)):
         for j in range(len(lista2)):
-            if lista1[i]==lista2[j]:
+            if lista1[i]==lista2[j] and (lista1[i] not in definitiva):
                 comunes.append(lista1[i])
-    return comunes
-
+                definitiva.append(lista1[i])       
+    return definitiva
 print(intersect(lista1, lista2))
-                
