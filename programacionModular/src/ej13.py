795133e8cf0b21fc27deda610160ec4eeab9e11b
Alejandra Mensaque <99336987+alemenfon@users.noreply.github.com>
2022-11-16 20:54:50 +0100

    Update ej13.py

--- programacionModular/src/ej13.py
+++ programacionModular/src/ej13.py
@@ -3,14 +3,18 @@ Escribe una función que, dada una lista de nombres y una letra, devuelva una li
 con todos los nombres que empiezan por dicha letra. Debe validar los datos.
 '''
 
-nombres=["Alejandra","Rocío","Jose","Ana","Antonio"]
+nombres=["Alejandra","Rocío","Jose","Ana","Antonio","Laura","Alba"]
 letra="A"
 
 def empiezanPor(nombres,letra):
     nombresEmpiezan=[]
-    for nombre in nombres:
-        if letra in nombre:
-            nombresEmpiezan.append(nombre)
+    res="0123456789,.;:-_<>#~$%&/()=?¿¡!^[]{}*-+ªº"
+    if (letra in res):
+        print("Debe ser un caracter alfabetico")
+    else:
+        for nombre in nombres:
+            if letra in nombre[0]:
+                nombresEmpiezan.append(nombre)
     return nombresEmpiezan
 
 print(empiezanPor(nombres, letra))
